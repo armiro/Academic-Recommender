5bad1d07ea5a5de891077806e42fc1c40cb73755
armiro <arman.hgh.1374@gmail.com>
2024-04-22 19:57:01 -0400

    fix PEP8 issues detected by pylint

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,6 @@
+"""
+preprocessing functions for input dataset
+"""
 import re
 from nltk.corpus import stopwords
 from spellchecker import SpellChecker
@@ -7,15 +10,34 @@ nltk.download('stopwords', quiet=True)
 
 
 def str_to_list(input_str):
+    """
+    use regex to split string into list of substrings
+
+    :param input_str: string
+    :return: list
+    """
     delimiters = ', |; |-'
-    return [e for e in re.split(pattern=delimiters, string=input_str)]
+    return list(re.split(pattern=delimiters, string=input_str))
 
 
-def normalize(input_list):  # make all words lower and clean indents
+def normalize(input_list):
+    """
+    make all words lower and clean indents
+
+    :param input_list: list of strings
+    :return: list
+    """
     return [e.lower().strip() for e in input_list]
 
 
-def remove_stop_words_from(input_list, method):  # remove stop words like 'of', 'and'
+def remove_stop_words_from(input_list, method):
+    """
+    remove stop words like 'of' or 'and'
+
+    :param input_list: list of strings
+    :param method: string, splitting method to tokenize strings; ['phrase' or 'token']
+    :return: list
+    """
     stop_words = stopwords.words('english')
     if method == 'token':
         return [e for w in input_list for e in w.split() if e not in stop_words]  # & return tokens
@@ -26,7 +48,13 @@ def remove_stop_words_from(input_list, method):  # remove stop words like 'of', 
 
 
 # TODO: use local vocab to make the dictionary more robust
-def check_spells_in(input_list):  # check for any typos
+def check_spells_in(input_list):
+    """
+    check for any possible typos using spell checker
+
+    :param input_list: list of strings
+    :return: none
+    """
     speller = SpellChecker(language='en', distance=2, case_sensitive=False)
     for phrase in input_list:
         misspelled = speller.unknown(phrase.split())
@@ -35,12 +63,25 @@ def check_spells_in(input_list):  # check for any typos
             print('best correction:', speller.correction(misspelled.pop()))
 
 
-def split_into_tokens(input_list):  # convert list of interests into single words
+def split_into_tokens(input_list):
+    """
+    convert list of interests into single words
+
+    :param input_list: list of strings
+    :return: list
+    """
     return [token for word in input_list for token in word.split()]
 
 
 # TODO: remove punctuations, numbers, links
 def preprocess(input_str, method):
+    """
+    apply preprocessing steps on the input string
+
+    :param input_str: string
+    :param method: string, splitting method to tokenize strings; ['phrase' or 'token']
+    :return: list
+    """
     input_list = str_to_list(input_str)
     normalized_list = normalize(input_list)
     cleaned_list = remove_stop_words_from(normalized_list, method)
